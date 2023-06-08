dcb25842c90be3031b1a27f975342e61b07811de
nmmn <51752838+nmmn1004@users.noreply.github.com>
2023-06-08 15:34:08 +0900

    Update 010809_알파벳_찾기.java

--- 010809_알파벳_찾기.java
+++ 010809_알파벳_찾기.java
@@ -8,6 +8,8 @@ class Main {
 		
 		for(int i = 'a'; i <= 'z'; i++) {
 			System.out.print(s.indexOf(i));
+// 			(StringType).indexOf(char); 
+// 			char 문자형이 처음으로 나오는 인덱스 번호를 반환, 없으면 -1 반환
 			if(i < 'z') {
 				System.out.print(" ");
 			}
