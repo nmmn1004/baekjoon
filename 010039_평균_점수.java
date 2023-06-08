c1a036c75643462e29d4f083173ab105408de4b6
nmmn <51752838+nmmn1004@users.noreply.github.com>
2023-06-08 14:41:14 +0900

    Create 010039_평균_점수.java

---
+++ 010039_평균_점수.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Main {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int total = 0;
+		for(int i = 0; i < 5; i++) {
+			int score = sc.nextInt();
+			if(score < 40) {
+				score = 40;
+			}
+			total += score;
+		}
+		System.out.println(total / 5);
+		
+		sc.close();
+	}
+}
